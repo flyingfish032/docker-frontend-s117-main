2dc042641b74e07b7995fd2cf13ace97da66a90f
flyingfish032 <hussaincme@gmail.com>
2025-09-18 12:12:11 +0530

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -15,5 +15,5 @@ export const getProducts = async (category = "") => {
 
 // Get product image URL
 export const getProductImageUrl = (imagePath) => {
-  return `http://localhost:8080/api/products/images/${imagePath}`;
+  return `http://ec2-16-170-232-57.eu-north-1.compute.amazonaws.com/api/products/images/${imagePath}`;
 };
